5b955c1c8e44544660e7a5c8bbe9a28e665dbd0f
jinarma <jinarma@gmail.com>
2021-09-09 22:56:58 +0530

    first commit

---
+++ C/printingPatternUsingLoops.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+	int n, i, j, k, l=0;
+	scanf("%d", &n);
+	k=n*2-1;
+	for(i=k; i>0; i--){
+		for(j=k; j>0; j--){
+			if()
+			printf("%d", n);
+			if(j==1)
+				printf("\n");
+			else
+				printf(" ");
+		}
+	}
+}
