e21f7a3413abbe0a783f605747a9bc92350c831b
jinarma <jinarma@gmail.com>
2021-09-30 10:51:42 +0530

    completed

--- Python/anyOrAll.py
+++ Python/anyOrAll.py
@@ -1,9 +1,15 @@
-""" take an int input then write as many nums as the input. If all the taken nums are +ve then check if any of them are palindromic ints. If both conditions are satisfied then print Ture else false """
+""" take an int input then write as many nums as the input. If all the taken nums are +ve then check if any of them are palindromic ints. If both conditions are satisfied then print True else false """
 
 count = int(input())
 temp = input()
 num_list = temp.split(' ')
-for i in count:
-	if i//2 <= 3:
-		all(num_list)
-		pass
+temp = ''.join(num_list)
+num_list = list(temp)
+for i in num_list:
+	if int(i) < 0:
+		print(False)
+		exit()
+if num_list == num_list[::-1]:
+	print(True)
+	exit()
+print(False)
